b21e172ce9e76deaf22bb563e47f366b08798cae
ratnasai2206 <kothamasuvenkataratnasai@gmail.com>
2024-03-11 15:05:27 +0530

    Hotel Dao is Added

--- src/main/java/com/hotelbillapplication/hotelbillapplication/dao/HotelDao.java
+++ src/main/java/com/hotelbillapplication/hotelbillapplication/dao/HotelDao.java
@@ -1,5 +1,48 @@
 package com.hotelbillapplication.hotelbillapplication.dao;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.hotelbillapplication.hotelbillapplication.entity.Hotel;
+import com.hotelbillapplication.hotelbillapplication.repository.HotelRepository;
+
+@Repository
 public class HotelDao {
 
+	@Autowired
+	private HotelRepository hotelRepository;
+	
+	// to save item into the database
+	public Hotel saveItem(Hotel hotel) {
+		return hotelRepository.save(hotel);
+	}
+	
+	// to get a particular item from the database by using item Id
+	public Hotel getItem(int item_Id) {
+		Optional<Hotel> optional=hotelRepository.findById(item_Id);
+		if(optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+	}
+	
+	
+	//to get the list of items present in the database
+	public List<Hotel> getItemList(){
+		return hotelRepository.findAll();
+	}
+	
+	//to delete particular item from the database by using item Id
+	public boolean removeItem(int item_Id) {
+		Hotel hotel=getItem(item_Id);
+		if(hotel !=null) {
+			hotelRepository.delete(hotel);
+			return true;
+		}
+		return false;
+	}
+	
 }
