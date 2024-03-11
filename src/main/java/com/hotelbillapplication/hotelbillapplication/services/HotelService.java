73b21778bb695b4aed1dd2e0b203a9b9fc11ff4c
Bhavana-anu <anushalalam01@gmail.com>
2024-03-11 16:27:04 +0530

    added hotel dao

--- src/main/java/com/hotelbillapplication/hotelbillapplication/services/HotelService.java
+++ src/main/java/com/hotelbillapplication/hotelbillapplication/services/HotelService.java
@@ -1,6 +1,14 @@
 package com.hotelbillapplication.hotelbillapplication.services;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.hotelbillapplication.hotelbillapplication.dto.ResponseStructure;
+import com.hotelbillapplication.hotelbillapplication.entity.Item;
 
 public interface HotelService {
+
+	  ResponseEntity<ResponseStructure<List<Item>>> showAllFoodItems();
 
 }
