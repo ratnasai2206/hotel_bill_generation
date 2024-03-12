bb2aee30f5f5cc9524be099fa6b3df76d2dd1293
ratnasai2206 <kothamasuvenkataratnasai@gmail.com>
2024-03-12 10:21:19 +0530

    seed config has be updated

--- src/main/java/com/hotelbillapplication/hotelbillapplication/exception/ItemNotFoundException.java
+++ src/main/java/com/hotelbillapplication/hotelbillapplication/exception/ItemNotFoundException.java
@@ -1,6 +1,7 @@
 package com.hotelbillapplication.hotelbillapplication.exception;
 
 public class ItemNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
 	private String message;
 	public ItemNotFoundException(String message) {
 		this.message=message;
